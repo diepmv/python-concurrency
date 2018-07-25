0dee2f3ae84319ed855e2ce020b7bebda3309eb1
Mai Diep <diepmv@vccloud.vn>
2018-07-25 21:24:21 +0700

    add distributed

---
+++ distributed/celery/create_task.py
@@ -0,0 +1,7 @@
+'''
+https://www.linkedin.com/learning/python-parallel-programming-solutions/creating-a-task-with-celery
+'''
+
+
+#task.apply_async(args[, kwargs[, ...]]): this task send a task message
+#task.delay(*args, **kwargs) : shortcut to send a task message, but does not support execution options
---
+++ distributed/pyro4/pyro4.py
@@ -0,0 +1,5 @@
+'''
+Invokes a method of a remote object almost as if the object were local
+
+Can be traced from a conceptual point of view
+'''
